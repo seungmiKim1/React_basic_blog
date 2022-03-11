d45c2087b2cd5c22164e1d534f41f8be7c2f6fee
brandy <89903766+seungmiKim1@users.noreply.github.com>
2022-03-11 15:20:59 +0900

    0311_약간의 변경

--- App.js
+++ App.js
@@ -3,6 +3,8 @@
 import React, { useState } from 'react'; // 리액트 내장함수인 state를 갖다쓰겠습니다
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
 
 
@@ -14,45 +16,50 @@ function App() {
   // 자주 바뀌는, 중요한 데이터를 변수 말고 state에 저장해서 쓸 것 
   // (블로그 제목은 수시로 바뀌지 않으니 state로 할 필요가 없다)
 
-  let [글제목,글제목변경] = useState(['강남 맛집 추천','강남 우동 맛집','오늘은 날씨가 꾸리꾸리합니다']); // a,b변수를 만들겠습니다 useState에 있던 변수 2개 각각 저장 a에는 실제 데이터, b에는 변경할 데이터
+  let [글제목,글제목변경] = useState(['처음 하는 철학 공부','나는 당신이 아픈 게 싫습니다','어쩌다 정신과 의사']); // a,b변수를 만들겠습니다 useState에 있던 변수 2개 각각 저장 a에는 실제 데이터, b에는 변경할 데이터
   let [추천,추천변경]=useState(0); // useState(1번째 변수의 초기값 : 따봉의 초기값)
   let [추천2,추천변경2]=useState(0);
   let [추천3,추천변경3]=useState(0);
   let [반대,반대변경]=useState(0);
   let [반대2,반대변경2]=useState(0);
   let [반대3,반대변경3]=useState(0);
-  let posts = '오늘의 맛집'; // 데이터 바인딩 : 데이터를 HTML에 꽂는 것 
-  //따봉변경(10); // 따봉 state가 10이 된다. 따봉변경(대체할 데이터)
+  //let posts = '오늘의 맛집'; // 데이터 바인딩 : 데이터를 HTML에 꽂는 것 
+  //추천변경(10); // 따봉 state가 10이 된다. 따봉변경(대체할 데이터)
 
   return (
     <div className="App">
      <div className="black-nav">
-       <div style={{color:'blue', fontSize:'30px'}}>
-      개발 Blog
+       <div style={{color:'skyblue', fontSize:'30px'}}>
+        B O O K L O G 
         </div>
       </div>
       
       
     <div className="list">
-     {/* <- JSX 코드 주석 : 따봉 누를때마다 1 증가시키기 state로 만드는게 좋음*/}
+     {/* <- JSX 코드 주석 : 추천 누를때마다 1 증가시키기 state로 만드는게 좋음*/}
       <h3>{ 글제목[0] }   <span onClick={()=>{추천변경(추천+1)}}>👍</span> {추천} <span onClick={()=>{반대변경(반대+1)}}>👎</span> {반대} </h3>
-      {/*따봉 누를때마다 콘솔창에 1 출력시키기
+      {/* <div>
+        <Link to="./https://blog.naver.com/tmdal0303/222668956190">
+         
+          </Link>
+      </div>     
+      {/*추천 누를때마다 콘솔창에 1 출력시키기
       <span onClick={()=> {console.log(1)}}>👍</span>0</h3>*/}
+      
      
-     
-      <p>3월 8일 발행</p>
+      <p>2022년 3월 8일 발행</p>
       <hr/>  
     </div>
 
     <div className="list">
       <h3>{ 글제목[1] }  <span onClick={()=>{추천변경2(추천2+1)}}>👍</span> {추천2} <span onClick={()=>{반대변경2(반대2+1)}}>👎</span> {반대2} </h3>
-      <p>3월 8일 발행</p>
+      <p>2022년 2월 18일 발행</p>
       <hr/>  
     </div>
 
     <div className="list">
       <h3>{ 글제목[2] }   <span onClick={()=>{추천변경3(추천3+1)}}>👍</span> {추천3} <span onClick={()=>{반대변경3(반대3+1)}}>👎</span> {반대3} </h3>
-      <p>3월 8일 발행</p>
+      <p>2022년 2월 14일 발행</p>
       <hr/>  
     </div>
   </div>
